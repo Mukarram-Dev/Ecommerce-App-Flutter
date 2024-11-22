e7290dc9e1a582d8bc706011080354730806ebcb
Mukarram Ali <bossmukaram@gmail.com>
2024-11-23 03:16:31 +0500

    product card responsive

--- lib/views/home/widgets/product_card_widget.dart
+++ lib/views/home/widgets/product_card_widget.dart
@@ -1,15 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:ecommerce_app/config/theme/colors.dart';
 import 'package:ecommerce_app/config/theme/text_theme_style.dart';
 import 'package:ecommerce_app/models/product.dart';
 import 'package:ecommerce_app/utils/extensions.dart';
 import 'package:ecommerce_app/utils/gaps.dart';
-import 'package:ecommerce_app/utils/size_config.dart';
-import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
   final ValueChanged<Product> onSelected;
   final VoidCallback onFavouritePress;
+
   const ProductCard({
     super.key,
     required this.product,
@@ -19,22 +19,20 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SizeConfig().init(context);
     return Container(
-      height: SizeConfig.screenHeight! * 2,
       decoration: const BoxDecoration(
         color: AppColors.white,
         borderRadius: BorderRadius.all(Radius.circular(20)),
         boxShadow: <BoxShadow>[
           BoxShadow(
-              color: AppColors.primaryLightColor,
-              blurRadius: 5,
-              spreadRadius: 1,
-              offset: Offset(0, 3)),
+            color: AppColors.primaryLightColor,
+            blurRadius: 5,
+            spreadRadius: 1,
+            offset: Offset(0, 3),
+          ),
         ],
       ),
       child: Column(
-        mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _buildTopImage(),
@@ -50,22 +48,27 @@ class ProductCard extends StatelessWidget {
       children: <Widget>[
         Container(
           width: double.infinity,
-          height: SizeConfig.screenHeight! * .17,
+          height: 160,
           alignment: Alignment.center,
           decoration: BoxDecoration(
-              color: AppColors.primaryColor.withAlpha(100),
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              )),
+            color: AppColors.primaryColor.withAlpha(100),
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
           child: Center(
-            child: Image.asset(product.image),
+            child: Image.asset(
+              product.image,
+              fit: BoxFit.contain,
+            ),
           ),
         ),
-        InkWell(
-          onTap: onFavouritePress,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
+        Positioned(
+          top: 8,
+          right: 8,
+          child: InkWell(
+            onTap: onFavouritePress,
             child: Icon(
               product.isliked ? Icons.favorite : Icons.favorite_border,
               color: product.isliked ? AppColors.red : AppColors.black,
@@ -87,21 +90,22 @@ class ProductCard extends StatelessWidget {
             product.name,
             style: AppTextStyles.textHeading3(
               color: AppColors.black,
-              fontSize: 18,
+              fontSize: 16, // Adjust font size if needed
             ),
           ),
           Gaps.verticalGapOf(5),
           Text(
             '\$${product.price.toString()}',
             style: AppTextStyles.textLabel(
-              fontSize: 20,
+              fontSize: 18,
               color: AppColors.primaryColor,
             ),
           ),
         ],
+      ).ripple(
+        () => onSelected(product),
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
       ),
-    ).ripple(() {
-      onSelected(product);
-    }, borderRadius: const BorderRadius.all(Radius.circular(20)));
+    );
   }
 }
--- lib/views/home/widgets/product_listview.dart
+++ lib/views/home/widgets/product_listview.dart
@@ -1,9 +1,8 @@
-import 'package:ecommerce_app/providers/home/home_provider.dart';
-import 'package:ecommerce_app/utils/size_config.dart';
-import 'package:ecommerce_app/views/details/details_screen.dart';
-import 'package:ecommerce_app/views/home/widgets/product_card_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ecommerce_app/providers/home/home_provider.dart';
+import 'package:ecommerce_app/views/details/details_screen.dart';
+import 'package:ecommerce_app/views/home/widgets/product_card_widget.dart';
 
 class ProductListview extends ConsumerWidget {
   const ProductListview({super.key});
@@ -16,36 +15,48 @@ class ProductListview extends ConsumerWidget {
         ? const Center(
             child: Text('No Product Found'),
           )
-        : Container(
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            width: SizeConfig.screenWidth,
-            child: GridView.builder(
-              itemCount: productList.length,
-              itemBuilder: (context, index) => ProductCard(
-                product: productList[index],
-                onSelected: (model) {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => DetailsScreen(
-                      product: productList[index],
-                    ),
-                  ));
-                },
-                onFavouritePress: () => ref
-                    .read(homeProvider.notifier)
-                    .updateFavouriteList(
-                        productList[index].id, !productList[index].isliked),
-              ),
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 2 / 3,
-                mainAxisSpacing: 30,
-                crossAxisSpacing: 20,
-              ),
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              scrollDirection: Axis.vertical,
-            ),
+        : LayoutBuilder(
+            builder: (context, constraints) {
+              final screenWidth = constraints.maxWidth;
+              const crossAxisCount = 2;
+              final cardWidth = (screenWidth - 40) / crossAxisCount;
+              final cardHeight = cardWidth * 1.55;
+              final childAspectRatio = cardWidth / cardHeight;
+
+              return Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                width: screenWidth,
+                child: GridView.builder(
+                  itemCount: productList.length,
+                  itemBuilder: (context, index) => ProductCard(
+                    product: productList[index],
+                    onSelected: (model) {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => DetailsScreen(
+                            product: productList[index],
+                          ),
+                        ),
+                      );
+                    },
+                    onFavouritePress: () =>
+                        ref.read(homeProvider.notifier).updateFavouriteList(
+                              productList[index].id,
+                              !productList[index].isliked,
+                            ),
+                  ),
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: crossAxisCount,
+                    mainAxisSpacing: 30,
+                    crossAxisSpacing: 20,
+                    childAspectRatio: childAspectRatio,
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                ),
+              );
+            },
           );
   }
 }
